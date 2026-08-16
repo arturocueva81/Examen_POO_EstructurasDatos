f4605846ae243dd55049b14571798ebb3f683150
arturocueva81 <ajcs81@hotmail.com>
2026-08-15 21:16:25 -0500

    Parte7: Metodos de negocio, metodos registrarVehiculo, recargarTag,CobrarPeaje,TransferirSaldoTag

--- src/com/krakedev/peaje/servicios/EstacionPeaje.java
+++ src/com/krakedev/peaje/servicios/EstacionPeaje.java
@@ -1,5 +1,10 @@
 package com.krakedev.peaje.servicios;
 
+import com.krakedev.peaje.entidades.Conductor;
+import com.krakedev.peaje.entidades.TagElectronico;
+import com.krakedev.peaje.entidades.Vehiculo;
+import com.krakedev.peaje.util.ValidadorUtil;
+
 public class EstacionPeaje {
 	
 	//Parte6: atributos con valores
@@ -34,6 +39,84 @@ public class EstacionPeaje {
 
 	public void setTarifaPesado(double tarifaPesado) {
 		this.tarifaPesado = tarifaPesado;
+	}
+	
+	//Parte7: Metodos de negocio
+	//registrarVehiculo
+	public Vehiculo registrarVehiculo(String placa, String tipo, Conductor conductor, String idTag) {
+	    ValidadorUtil validador = new ValidadorUtil();
+	    boolean tipoValido = validador.esTipoValido(tipo);
+	    
+	    if (tipoValido == true) {
+	        Vehiculo vehiculo = new Vehiculo(placa);
+	        vehiculo.setTipo(tipo);
+	        vehiculo.setPropietario(conductor);
+	        TagElectronico tag = new TagElectronico(idTag);
+	        vehiculo.setTag(tag);
+	        return vehiculo;
+	    } else {
+	        return null;
+	    }
+	}
+	
+	//recargarTag
+	public boolean recargarTag(double monto, TagElectronico tag) {
+		ValidadorUtil validador = new ValidadorUtil();
+	    boolean montoValido = validador.esMontoValido(monto);
+	    
+	    if (montoValido == true) {
+	        tag.setSaldo(tag.getSaldo() + monto);
+	        return true;
+	    } else {
+	        return false;
+	    }
+	}
+	
+	//cobrarPeaje
+	public boolean cobrarPeaje(Vehiculo vehiculo) {
+		ValidadorUtil validador = new ValidadorUtil();
+	    boolean tipoValido = validador.esTipoValido(vehiculo.getTipo());
+	    
+	    if (tipoValido == false) {
+	        return false;
+	    }
+	    
+	    double tarifa;
+	    
+	    if (vehiculo.getTipo().equals("L")) {
+	        tarifa = tarifaLiviano;
+	    } else {
+	        tarifa = tarifaPesado;
+	    }
+	    
+	    boolean saldoSuficiente = vehiculo.getTag().getSaldo() >= tarifa;
+	    
+	    if (saldoSuficiente == true) {
+	        vehiculo.getTag().setSaldo(vehiculo.getTag().getSaldo() - tarifa);
+	        return true;
+	    } else {
+	        return false;
+	    }
+	}
+	
+	//transferirSaldoTag
+	public boolean transferirSaldoTag(TagElectronico origen, TagElectronico destino, double monto) {
+		ValidadorUtil validador = new ValidadorUtil();
+	    boolean montoValido = validador.esMontoValido(monto);
+	    
+	    if (montoValido == false) {
+	        return false;
+	    }
+	    
+	    boolean saldoSuficiente = monto <= origen.getSaldo();
+	    
+	    if (saldoSuficiente == true) {
+	        origen.setSaldo(origen.getSaldo() - monto);
+	        destino.setSaldo(destino.getSaldo() + monto);
+	        return true;
+	    } else {
+	        return false;
+	    }
 	}
 
 }
