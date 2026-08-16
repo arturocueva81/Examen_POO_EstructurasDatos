ddef668d5344eaafb44f6d056c6b15c80a12cd24
arturocueva81 <ajcs81@hotmail.com>
2026-08-15 20:48:40 -0500

    Parte4: Clase ImpresorUtil , imprime informacion de los objetos compuestos

---
+++ src/com/krakedev/peaje/util/ImpresorUtil.java
@@ -0,0 +1,16 @@
+package com.krakedev.peaje.util;
+
+import com.krakedev.peaje.entidades.Vehiculo;
+
+public class ImpresorUtil {
+	
+	public void imprimirVehiculo(Vehiculo vehiculo) {
+		//Parte 4 mostrar informacion de los objetos compuestos
+		System.out.println("INFORMACION DEL VEHICULO");
+		vehiculo.imprimir();
+		vehiculo.getPropietario().imprimir();
+		vehiculo.getTag().imprimir();
+		System.out.println("");		
+		
+	}
+}
